ec0223aa45cccfae1bb9740a6047e9226e6e32b2
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-12-11 15:30:23 -0700

    coded while loops and how they work

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WhileLoops_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Type your Name");
+                var input = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(input))
+                    break;
+
+                Console.WriteLine("@Echo: " + input);
+            }
+
+            Console.WriteLine("Loop ended.");
+
+            // Uncomment the following code to see the while loop in action
+
+            // var i = 0;
+            // while (i <= 10)
+            // {
+            //     if (i % 2 == 0)
+            //         Console.WriteLine(i);
+            //
+            //     i++;
+            // }
+        }
+    }
+}
